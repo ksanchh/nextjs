dd8ee9e93853e7351a58653ffe25434a4ba552db
Kevin Sanchez <kevin@runadic.com>
2023-12-01 15:04:17 +0000

    test commit

--- src/component/banner/BannerSection.tsx
+++ src/component/banner/BannerSection.tsx
@@ -8,7 +8,7 @@ const BannerSection = () => {
             <div className="row gy-5 tl-1-banner-top pt-100 pb-100 align-items-center">
                 <div className="col-lg-7">
                     <div className="tl-1-banner-txt">
-                        <h1 className="tl-1-banner-title">Learning Management System (LMS).</h1>
+                        <h1 className="tl-1-banner-title">Kevin</h1>
                         <p className="tl-1-banner-descr">Through a combination of lectures, readings, and discussions, students will gain a solid foundation in educational psychology.</p>
                         <div className="tl-1-banner-btns">
                             <a href="#" className="tl-5-def-btn tl-1-banner-btn-1">I Want a Demo</a>
